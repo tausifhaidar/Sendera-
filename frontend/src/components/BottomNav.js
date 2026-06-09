81290cf68e72dc18fc373d281f37e2c54b4f67a4
Taushif <tausifhaidar63@gmail.com>
2026-06-09 16:35:26 +0530

    Simplify BottomNav component
    Refactor BottomNav component to a simpler version.

--- frontend/src/components/BottomNav.js
+++ frontend/src/components/BottomNav.js
@@ -1,55 +1,5 @@
-function BottomNav({
-  activeTab,
-  setActiveTab,
-}) {
-  return (
-    <div
-      style={{
-        position: "fixed",
-        bottom: 0,
-        left: 0,
-        right: 0,
-        background: "#0f172a",
-        display: "flex",
-        justifyContent: "space-around",
-        padding: 15,
-        borderTop:
-          "1px solid #334155",
-      }}
-    >
-      <button
-        onClick={() =>
-          setActiveTab("home")
-        }
-      >
-        Home
-      </button>
-
-      <button
-        onClick={() =>
-          setActiveTab("receive")
-        }
-      >
-        Receive
-      </button>
-
-      <button
-        onClick={() =>
-          setActiveTab("history")
-        }
-      >
-        History
-      </button>
-
-      <button
-        onClick={() =>
-          setActiveTab("settings")
-        }
-      >
-        Settings
-      </button>
-    </div>
-  );
+function BottomNav() {
+  return <div>Bottom Nav</div>;
 }
 
 export default BottomNav;
