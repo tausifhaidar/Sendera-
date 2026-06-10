cb90a6bd75fb4796702b8645be05474fb648260b
Taushif <tausifhaidar63@gmail.com>
2026-06-10 13:34:20 +0530

    Create PortfolioCard component with basic layout

--- frontend/src/components/PortfolioCard.js
+++ frontend/src/components/PortfolioCard.js
@@ -1,5 +1,20 @@
-function FileName() {
-  return null;
+function PortfolioCard() {
+  return (
+    <div
+      style={{
+        background: "#0f172a",
+        padding: 20,
+        borderRadius: 16,
+        marginTop: 20,
+      }}
+    >
+      <h3>Portfolio</h3>
+
+      <p>Ethereum: $0.00</p>
+      <p>Base: $0.00</p>
+      <p>Polygon: $0.00</p>
+    </div>
+  );
 }
 
-export default FileName;
+export default PortfolioCard;
