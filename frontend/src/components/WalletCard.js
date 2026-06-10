44fe4438a160622465ee7a722fdcf1cb983b2e48
Taushif <tausifhaidar63@gmail.com>
2026-06-10 13:18:58 +0530

    Implement WalletCard component for wallet display

--- frontend/src/components/WalletCard.js
+++ frontend/src/components/WalletCard.js
@@ -1,5 +1,42 @@
-function FileName() {
-  return null;
+function WalletCard({ wallet }) {
+  return (
+    <div
+      style={{
+        background: "#0f172a",
+        padding: 20,
+        borderRadius: 16,
+        marginTop: 20,
+      }}
+    >
+      <p
+        style={{
+          color: "#94a3b8",
+          marginBottom: 10,
+        }}
+      >
+        Sendera Wallet
+      </p>
+
+      <h3
+        style={{
+          margin: 0,
+          wordBreak: "break-all",
+        }}
+      >
+        {wallet?.address ||
+          "No Wallet"}
+      </h3>
+
+      <p
+        style={{
+          marginTop: 15,
+          color: "#22c55e",
+        }}
+      >
+        Base Sepolia
+      </p>
+    </div>
+  );
 }
 
-export default FileName;
+export default WalletCard;
