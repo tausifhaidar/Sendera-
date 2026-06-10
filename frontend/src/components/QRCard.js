f6648dde213bea026b4137fd71aa0f3ff4f838d4
Taushif <tausifhaidar63@gmail.com>
2026-06-10 14:02:03 +0530

    Rename component to QRCard and implement UI

--- frontend/src/components/QRCard.js
+++ frontend/src/components/QRCard.js
@@ -1,5 +1,33 @@
-function FileName() {
-  return null;
+function QRCard() {
+return (
+<div
+style={{
+background: "#0f172a",
+padding: 20,
+borderRadius: 16,
+marginTop: 20,
+textAlign: "center",
+}}
+>
+<h3>Wallet QR</h3>
+
+  <div
+    style={{
+      width: 180,
+      height: 180,
+      background: "#1e293b",
+      margin: "20px auto",
+      borderRadius: 12,
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    }}
+  >
+    QR Coming Soon
+  </div>
+</div>
+
+);
 }
 
-export default FileName;
+export default QRCard;
