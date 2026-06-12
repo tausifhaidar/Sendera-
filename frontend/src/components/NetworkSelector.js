e6d74128b39c685544a2ba299f5f00321ac9f1b0
Taushif <tausifhaidar63@gmail.com>
2026-06-13 01:15:51 +0530

    Implement NetworkSelector component

--- frontend/src/components/NetworkSelector.js
+++ frontend/src/components/NetworkSelector.js
@@ -1,5 +1,27 @@
-function FileName() {
-  return null;
+function NetworkSelector() {
+return (
+<div
+style={{
+background: "#0f172a",
+padding: 20,
+borderRadius: 16,
+marginTop: 20,
+}}
+>
+<h3>Network Selector</h3>
+
+  <div
+    style={{
+      marginTop: 15,
+    }}
+  >
+    <p>🟢 Base Sepolia</p>
+    <p>⚪ Ethereum Sepolia</p>
+    <p>⚪ Polygon Amoy</p>
+  </div>
+</div>
+
+);
 }
 
-export default FileName;
+export default NetworkSelector;
