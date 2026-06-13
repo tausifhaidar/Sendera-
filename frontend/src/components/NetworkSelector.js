e58f151431c68d983211ca452a783020a4f8b456
Taushif <tausifhaidar63@gmail.com>
2026-06-13 15:50:22 +0530

    Refactor NetworkSelector to use props for networks

--- frontend/src/components/NetworkSelector.js
+++ frontend/src/components/NetworkSelector.js
@@ -1,27 +1,60 @@
-function NetworkSelector() {
-return (
-<div
-style={{
-background: "#0f172a",
-padding: 20,
-borderRadius: 16,
-marginTop: 20,
-}}
->
-<h3>Network Selector</h3>
+function NetworkSelector({
+  selectedNetwork,
+  setSelectedNetwork,
+}) {
+  const networks = [
+    {
+      key: "baseSepolia",
+      name: "Base Sepolia",
+    },
+    {
+      key: "ethereumSepolia",
+      name: "Ethereum Sepolia",
+    },
+    {
+      key: "polygonAmoy",
+      name: "Polygon Amoy",
+    },
+  ];
 
-  <div
-    style={{
-      marginTop: 15,
-    }}
-  >
-    <p>🟢 Base Sepolia</p>
-    <p>⚪ Ethereum Sepolia</p>
-    <p>⚪ Polygon Amoy</p>
-  </div>
-</div>
+  return (
+    <div
+      style={{
+        background: "#0f172a",
+        padding: 20,
+        borderRadius: 16,
+        marginTop: 20,
+      }}
+    >
+      <h3>Network Selector</h3>
 
-);
+      <div
+        style={{
+          marginTop: 15,
+        }}
+      >
+        {networks.map((network) => (
+          <p
+            key={network.key}
+            onClick={() =>
+              setSelectedNetwork(
+                network.key
+              )
+            }
+            style={{
+              cursor: "pointer",
+            }}
+          >
+            {selectedNetwork ===
+            network.key
+              ? "🟢"
+              : "⚪"}{" "}
+            {network.name}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default NetworkSelector;
