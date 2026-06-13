b35e3e85305fbf9db97e7498395d0243f00ec8a5
Taushif <tausifhaidar63@gmail.com>
2026-06-13 14:31:53 +0530

    Add NETWORKS configuration for various blockchains

---
+++ frontend/src/components/rocConfig.js
@@ -0,0 +1,16 @@
+export const NETWORKS = {
+  baseSepolia: {
+    name: "Base Sepolia",
+    rpc: "https://sepolia.base.org",
+  },
+
+  ethereumSepolia: {
+    name: "Ethereum Sepolia",
+    rpc: "https://rpc.sepolia.org",
+  },
+
+  polygonAmoy: {
+    name: "Polygon Amoy",
+    rpc: "https://rpc-amoy.polygon.technology",
+  },
+};
