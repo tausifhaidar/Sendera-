0c74b9543e25eeb5b94a40dde8377971a8ecc0aa
Taushif <tausifhaidar63@gmail.com>
2026-06-15 13:44:28 +0530

    Implement TransactionPreview component

--- frontend/src/components/TransactionPreview.js
+++ frontend/src/components/TransactionPreview.js
@@ -1,5 +1,27 @@
-function FileName() {
-  return null;
+function TransactionPreview({
+  address,
+  amount,
+}) {
+  return (
+    <div
+      style={{
+        background: "#0f172a",
+        padding: 20,
+        borderRadius: 16,
+        marginTop: 20,
+      }}
+    >
+      <h3>Transaction Preview</h3>
+
+      <p>
+        To: {address || "No Address"}
+      </p>
+
+      <p>
+        Amount: {amount || "0"} ETH
+      </p>
+    </div>
+  );
 }
 
-export default FileName;
+export default TransactionPreview;
