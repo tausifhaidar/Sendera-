685aa7e37a315eebbe1b818364629e08e8eea772
Taushif <tausifhaidar63@gmail.com>
2026-07-05 16:34:19 +0530

    Add selectedNetwork and setBalance props to component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -252,6 +252,8 @@ function App() {
             setSendAmount={setSendAmount}
             showPreview={showPreview}
             setShowPreview={setShowPreview}
+            selectedNetwork={selectedNetwork}
+            setBalance={setBalance}
             />
           )}
           
